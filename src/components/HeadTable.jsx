59f2c2f17d4ee35b28328815d52988e77c353cd0
SandyDSM <sad.zoe@gmail.com>
2023-01-29 17:01:32 -0600

    Estilos a tabla de evaliación

--- src/components/HeadTable.jsx
+++ src/components/HeadTable.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function HeadTable() {
   return (
     <div className="flex flex-col text-bmb-blue font-medium">
-      <div className="flex flex-row self-stretch py-2 px-5 items-center relative gap-12 pr-10">
+      <div className="flex flex-row self-stretch py-2 px-5 items-center relative gap-8 pr-10">
       <p className="text-sm">Notificar</p>
       <p className="text-sm">Estatus</p>
         <div className="flex flex-col grow shrink relative whitespace-pre-wrap">
--- src/components/TableRowTests.jsx
+++ src/components/TableRowTests.jsx
@@ -1,24 +1,33 @@
 import { Divider, SwitchField } from "@aws-amplify/ui-react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IconNotification } from "../ui-components";
 
-function TableRowTests({anio}) {
+function TableRowTests({ anio }) {
+  const [isChecked, setIsChecked] = useState(false);
 
-  const [isChecked, setIsChecked] = useState(true);
+  useEffect(() => {
+    if (anio.ACTIVO === "1") {
+      setIsChecked(true);
+    } else {
+      setIsChecked(false);
+    }
+  }, []);
+
   return (
     <div className="flex flex-col">
-      
-              <div className="flex flex-row self-stretch py-2 px-9 items-center relative gap-4">
+      <div className="flex flex-row self-stretch py-2 px-9 items-center relative gap-1">
         <div className="w-7 h-7">
-          <IconNotification width="24px" height="24px" disabled={`${isChecked ? "False" : "True"}`} />
+          <IconNotification
+            width="24px"
+            height="24px"
+            disabled={`${isChecked ? "False" : "True"}`}
+          />
         </div>
         <div className="flex flex-col gap-2.5 justify-center items-center py-2 px-9 relative">
           <SwitchField
-          
             shrink="0"
             size="default"
-            defaultChecked={anio.ACTIVO==="1" && "Checked"}
-            
+            defaultChecked={anio.ACTIVO === "1" && "Checked"}
             onChange={(e) => {
               setIsChecked(e.target.checked);
             }}
@@ -26,7 +35,7 @@ function TableRowTests({anio}) {
         </div>
         <div className="flex flex-col grow shrink relative whitespace-pre-wrap">
           <p className="text-sm">
-            Evaluación de desempeño y sucesión {anio.ANIO} 
+            Evaluación de desempeño y sucesión {anio.ANIO}
           </p>
         </div>
         <p className="text-sm">{anio.ANIO}</p>
@@ -39,7 +48,7 @@ function TableRowTests({anio}) {
         size="small"
         orientation="horizontal"
       />
-</div>
+    </div>
   );
 }
 
