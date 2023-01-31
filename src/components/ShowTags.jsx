f1353d3d80322eb722bba8b9b35fba669bb495ae
SandyDSM <sad.zoe@gmail.com>
2023-01-30 23:15:35 -0600

    Quitar scroll horizontal

--- src/components/ShowTags.jsx
+++ src/components/ShowTags.jsx
@@ -6,7 +6,7 @@ import { Heading, Divider } from "@aws-amplify/ui-react";
 
 function ShowTags() {
   return (
-    <div className='card flex flex-col gap-6 p-4 w-full'>
+    <div className='card flex flex-col gap-6 p-4 w-full mb-8'>
         <div className="flex flex-col gap-1 my-2">
         <Heading level={6}> Etiquetas</Heading>
         <Divider />
