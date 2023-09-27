92934d208763d39ed5f6ed5cac406f516ed0445a
SandyDSM <sad.zoe@gmail.com>
2023-09-26 22:33:38 -0600

    Agregar Id a tabla de permissions

--- src/components/HeadTableColl.jsx
+++ src/components/HeadTableColl.jsx
@@ -5,7 +5,8 @@ function HeadTableColl() {
   return (
     <div className="flex flex-col text-bmb-blue font-medium">
       <div className="flex flex-row self-stretch py-2  items-center justify-between gap-5">
-        <div className="w-80"><p className="text-sm">Agregar</p></div>
+        <div className="w-1/3"><p className="text-sm text-center">Agregar</p></div>
+        <div className="w-2/5"><p className="text-sm">ID </p></div>
         <div className="w-full"><p className="text-sm">Nombre del colaborador</p></div>
         <div className="w-full"><p className="text-sm">Entidad legal</p></div>
         <div className="w-full"><p className="text-sm">Organización</p></div>
--- src/components/TableColl.jsx
+++ src/components/TableColl.jsx
@@ -6,7 +6,7 @@ function TableColl({colaborator, selectorg, add, indice}) {
   return (
     <div className="flex flex-col text-xs">
       <div className="flex flex-row self-stretch py-2  items-center justify-between gap-5">
-        <div className="w-80">
+        <div className="w-1/3">
           {add==="true" ? (<div className="flex flex-col m-auto h-6 cursor-pointer items-center" onClick={()=>selectorg(colaborator)}>
             <IconActions width="20px" height="20px"/>
           </div>) : (
@@ -16,6 +16,7 @@ function TableColl({colaborator, selectorg, add, indice}) {
           )}
           
         </div>
+        <div className="w-2/5"><p className="text-sm">{colaborator?.ID_COLABORADOR}</p></div>
         <div className="w-full"><p className="text-sm">{`${colaborator?.NOMBRE.toUpperCase()} ${colaborator?.APELLIDOS.toUpperCase()}`}</p></div>
         <div className="w-full"><p className="text-sm">{colaborator?.ENTIDAD_LEGAL}</p></div>
         <div className="w-full"><p className="text-sm">{colaborator?.ORGANIZACION}</p></div>
