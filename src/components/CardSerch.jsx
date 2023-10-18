2fd53bdb6934417dee479199ab154b13fc4922d9
SandyDSM <sad.zoe@gmail.com>
2023-10-17 22:19:00 -0600

    Corrección de estilos mobile serch 2

--- src/components/CardSerch.jsx
+++ src/components/CardSerch.jsx
@@ -4,7 +4,7 @@ import { Divider } from '@aws-amplify/ui-react'
 
 const CardSerch = ({collaborators, setCloseSerchAll}) => {
   return (
-    <div className='w-11/12 md:w-96 overflow-y-auto max-h-96 absolute top-28 md:top-16 right-4 md:right-6 z-10 drop-shadow-md'>
+    <div className='w-11/12 md:w-96 overflow-y-auto max-h-96 absolute top-28 md:top-16 right-4 md:right-0 z-10 drop-shadow-md'>
     <div className='card flex flex-col justify-center gap-4 divide-y divide-blue-200'>
       {collaborators <= 0 ? <p className='col-span-3 items-center text-center mt-6 px-6'>No se encontraron colaboradores</p>
       :(
