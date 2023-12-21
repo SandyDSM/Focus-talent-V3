01d58f1b842e6f73a8059e8f5a9f7989d8d0d701
SandyDSM <sad.zoe@gmail.com>
2023-12-20 18:53:20 -0600

    Boton exportar excel

--- src/containers/ReportLogin.jsx
+++ src/containers/ReportLogin.jsx
@@ -3,11 +3,12 @@ import HeadAdmin from "../components/HeadAdmin";
 import CollaborLogin from "../components/CollaborLogin";
 import { API } from 'aws-amplify';
 import { useContext, useEffect, useState } from "react";
-/*import ExportExel from 'react-export-excel'
+import ExportExel from 'react-export-excel'
+import { Button } from "@aws-amplify/ui-react";
 
 const Excelfile = ExportExel.ExcelFile;
 const ExcelSheet = ExportExel.ExcelSheet;
-const ExcelColum = ExportExel.ExcelColum;*/
+const ExcelColum = ExportExel.ExcelColum;
 
 function ReportLogin() {
   const [tableAccess, setTableAccess] = useState([])
@@ -41,6 +42,17 @@ function ReportLogin() {
     <div className="flex flex-col gap-6">
     <HeadAdmin title={"Reporte de Accesos"} />
     <div className='px-11'>
+    <div className="card grid justify-items-end mb-8">
+      <Excelfile element={<Button variation="primary">Exportar a excel</Button>} filename="Reporte de Accesos">
+        <ExcelSheet data={tableAccess} name="Reporte">
+          <ExcelColum label="ID Colaborador" value="ID_COLABORADOR"/>
+          <ExcelColum label="Nombre" value="NOMBRE"/>
+          <ExcelColum label="Apellido" value="APELLIDOS"/>
+          <ExcelColum label="Fecha de último acceso" value="FECHA_DE_ULTIMO_ACCESO"/>
+          <ExcelColum label="Total de accesos" value="NUMERO_DE_ACCESOS"/>
+        </ExcelSheet>
+      </Excelfile>
+      </div>
     <div className="card flex flex-col gap-6 w-full mb-8 overflow-x-scroll">
         <CollaborLogin tableAccess={tableAccess}/>
     </div>
